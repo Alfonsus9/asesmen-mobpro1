47a34105de1b26df7b9614bb1197b093ccbb9e1a
Alfonsus9 <bacon3845sus@gmail.com>
2026-05-12 15:35:34 +0700

    membuat database

--- app/src/main/java/com/alfonsus0031/dompetku/Model/Transaksi.kt
+++ app/src/main/java/com/alfonsus0031/dompetku/Model/Transaksi.kt
@@ -1,6 +1,12 @@
 package com.alfonsus0031.dompetku.Model
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "transaksi")
 data class Transaksi(
+    @PrimaryKey(autoGenerate = true)
+    val id: Long = 0L,
     val judul: String,
     val nominal: Int,
     val tipe: String,
---
+++ app/src/main/java/com/alfonsus0031/dompetku/database/TransaksiDao.kt
@@ -0,0 +1,26 @@
+package com.alfonsus0031.dompetku.database
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import com.alfonsus0031.dompetku.Model.Transaksi
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface TransaksiDao {
+    @Insert
+    suspend fun insert(transaksi: Transaksi)
+
+    @Update
+    suspend fun update(transaksi: Transaksi)
+
+    @Query("SELECT * FROM transaksi ORDER BY tanggal DESC")
+    fun getCatatan(): Flow<List<Transaksi>>
+
+    @Query("SELECT * FROM transaksi WHERE id = :id")
+    suspend fun getCatatanById(id: Long): Transaksi?
+
+    @Query("DELETE FROM transaksi WHERE id = :id")
+    suspend fun deleteById(id: Long)
+}
